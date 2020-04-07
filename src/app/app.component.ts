36cf23587a104303f7819fc073f58bddd110306f
VictorBarbosa <victor.hugo.p.barbosa@hotmail.com>
2020-04-06 21:02:31 -0400

    added opencv.js and wasm

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+declare const cv: any;
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-opencv';
+  constructor() {
+
+
+  }
+}
